513f2df9a3de26cca56d0b6b44e51eb8962a57be
Dan Actor <danltactor@gmail.com>
2018-09-15 16:36:26 -0600

    init

---
+++ gatsbyHTMLjs.js
@@ -0,0 +1,50 @@
+//for configuring how index.html is rendered
+
+import React from 'react';
+import favicon from './favicon.ico';
+
+let inlinedStyles = "";
+if (process.env.NODE_ENV === "production") {
+  try {
+    inlinedStyles = require("!raw-loader!../public/styles.css");
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === "production") {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: inlinedStyles }}
+        />
+      );
+    }
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {this.props.headComponents}
+          <link rel="shortcut icon" href={favicon} />
+          {css}
+        </head>
+        <body>
+          <div id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
+
+export default HTML;
+
